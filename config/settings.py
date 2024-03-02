d2e08af537842ad6c1d002200c5a5673bf833635
Pouria Shirali <impoush@gmail.com>
2024-03-02 17:10:06 +0330

    connect project to mysql db and migrate information to db and create superuser pouria

--- config/settings.py
+++ config/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "digikala_online_shop",
+        "HOST": "localhost",
+        "USER": "root",
+        "PASSWORD": "dKah40<6=c`+",
     }
 }
 
