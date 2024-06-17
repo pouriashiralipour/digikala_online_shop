563d3c49d751b2bc88c0860659aef19f3d9a4817
Pouria Shirali <impoush@gmail.com>
2024-06-17 21:36:18 +0330

    create registration view for login user

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.mobile_login_view, name="mobile_login"),
+    path("", views.register_view, name="register_view"),
+    # path("login/", views.mobile_login_view, name="mobile_login"),
     path("dashboard/", views.dashboard_view, name="dashboard"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -3,17 +3,41 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
+from .forms import CustomUserCreationForm
 from .models import CustomUser
 
 
-def mobile_login_view(request):
+def register_view(request):
+    form = CustomUserCreationForm
     if request.method == "POST":
-        if "phone_number" in request.POST:
-            phone_number = request.POST.get("phone_number")
-            user = CustomUser.objects.get(phone_number=phone_number)
-            login(request, user)
-            return HttpResponseRedirect(reverse("dashboard"))
-    return render(request, "login_signup.html")
+        try:
+            if "phone_number" in request.POST:
+                phone_number = request.POST.get("phone_number")
+                user = CustomUser.objects.get(phone_number=phone_number)
+                # send otp
+                # save otp
+                user.save()
+                # redirect to vrify page
+        except CustomUser.DoesNotExist:
+            form = CustomUserCreationForm(request.POST)
+            if form.is_valid():
+                user = form.save(commit=False)
+                # send otp
+                # save otp
+                user.is_active = False
+                user.save()
+                # redirect to vrify page
+    return render(request, "login_signup.html", {"form": form})
+
+
+# def mobile_login_view(request):
+#     if request.method == "POST":
+#         if "phone_number" in request.POST:
+#             phone_number = request.POST.get("phone_number")
+#             user = CustomUser.objects.get(phone_number=phone_number)
+#             login(request, user)
+#             return HttpResponseRedirect(reverse("dashboard"))
+#     return render(request, "login_signup.html")
 
 
 def dashboard_view(request):
