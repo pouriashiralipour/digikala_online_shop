4d152852621fd3f900a27b66e84429e8e7161fbb
Pouria Shirali <impoush@gmail.com>
2024-06-17 22:10:56 +0330

    create function for sen otp

--- accounts/otp_sender.py
+++ accounts/otp_sender.py
@@ -1,19 +1,28 @@
+from random import randint
+
 from django.conf import settings
 from kavenegar import *
 
 
 def send_otp(phone_number, otp):
+    phone_number = [
+        phone_number,
+    ]
     try:
         api = KavenegarAPI(settings.KAVEHNEGAR_API, timeout=20)
         params = {
-            "sender": "",  # optional
-            "receptor": "",  # multiple mobile number, split by comma
-            "message": "",
+            "sender": "1000596446",  # optional
+            "receptor": "phone_number",  # multiple mobile number, split by comma
+            "message": "Your OTP is {}".format(otp),
         }
         response = api.sms_send(params)
+        print("OTP", otp)
         print(response)
     except APIException as e:
         print(e)
     except HTTPException as e:
         print(e)
-    print("OTP", otp)
+
+
+def get_random_otp():
+    return randint(1000, 9999)
