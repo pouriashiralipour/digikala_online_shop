1633c14ebdc271b386984fd5f466d2b66526a051
Pouria Shirali <impoush@gmail.com>
2024-06-17 20:15:32 +0330

    add otp created time field for expire code

--- accounts/models.py
+++ accounts/models.py
@@ -6,3 +6,4 @@ class CustomUser(AbstractUser):
     username = None
     phone_number = models.CharField(max_length=11, unique=True)
     otp = models.PositiveIntegerField(blank=True, null=True)
+    otp_datetime_created = models.DateTimeField(auto_now=True)
