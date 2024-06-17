d3c0196e8a7514481ac0b9213a47bca8b6197b08
Pouria Shirali <impoush@gmail.com>
2024-06-17 21:26:39 +0330

    add forms for login

---
+++ accounts/forms.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+
+from .models import CustomUser
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = [
+            "phone_number",
+        ]
