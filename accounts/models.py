04b8fed6dca47794e5eaf5b024b33639614a924b
Pouria Shirali <impoush@gmail.com>
2024-06-17 20:14:42 +0330

    add otp field for sen code for login

--- accounts/models.py
+++ accounts/models.py
@@ -5,3 +5,4 @@ from django.db import models
 class CustomUser(AbstractUser):
     username = None
     phone_number = models.CharField(max_length=11, unique=True)
+    otp = models.PositiveIntegerField(blank=True, null=True)
