67262d865a7784aee973f0ce6821015bf12fe501
Pouria Shirali <impoush@gmail.com>
2024-06-17 20:19:13 +0330

    add custom user manager to models

--- accounts/models.py
+++ accounts/models.py
@@ -1,12 +1,16 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from .custom_user_manager import CustomUserManager
+
 
 class CustomUser(AbstractUser):
     username = None
     phone_number = models.CharField(max_length=11, unique=True)
     otp = models.PositiveIntegerField(blank=True, null=True)
     otp_datetime_created = models.DateTimeField(auto_now=True)
+
+    objects = CustomUserManager
 
     USERNAME_FIELD = "phone_number"
 
