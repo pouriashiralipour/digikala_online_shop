37f5218bebf28d77c3169ba0505ea908d036ff87
Pouria Shirali <impoush@gmail.com>
2024-06-17 20:54:35 +0330

    create views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,20 @@
+from django.contrib.auth import login
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
-# Create your views here.
+from .models import CustomUser
+
+
+def mobile_login_view(request):
+    if request.method == "POST":
+        if "phone_number" in request.POST:
+            phone_number = request.POST.get("phone_number")
+            user = CustomUser.objects.get(phone_number=phone_number)
+            login(request, user)
+            return HttpResponseRedirect(reverse("dashboard"))
+    return render(request, "login_signup.html")
+
+
+def dashboard_view(request):
+    return render(request, "dashboard.html")
