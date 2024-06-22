8047e1dceae8ca2dd54dedd32feaa3d2bb3033b2
Pouria Shirali <impoush@gmail.com>
2024-06-22 11:30:09 +0330

    change url name of login user to site

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,8 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.register_view, name="register_view"),
-    path("verify/", views.verify_view, name="verify"),
-    # path("login/", views.mobile_login_view, name="mobile_login"),
-    path("dashboard/", views.dashboard_view, name="dashboard"),
+    path("login/", views.register_view, name="register_view"),
+    path("verify/", views.verify_view, name="verify_view"),
+    path("welcome/", views.welcome_view, name="welcome_view"),
 ]
