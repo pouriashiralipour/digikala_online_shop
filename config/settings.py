1b46b8bc0ccb7107e06922198a93e5db83f54b9a
Pouria Shirali <impoush@gmail.com>
2024-06-23 06:29:30 +0330

    add static root line for add staticfiled

--- config/settings.py
+++ config/settings.py
@@ -142,6 +142,7 @@ USE_TZ = False
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [str(BASE_DIR.joinpath("static"))]
+# STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
