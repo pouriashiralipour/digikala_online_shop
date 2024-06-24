d61754b6dbd3f98d2bb670d7143dcc5bc97447c3
Pouria Shirali <impoush@gmail.com>
2024-06-24 11:45:37 +0330

    add url in pages app to config urls

--- config/urls.py
+++ config/urls.py
@@ -21,5 +21,6 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
+    path("", include("pages.urls")),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
